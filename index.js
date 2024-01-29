adf875d5f00e367ad20c7a07a007d0180e27c38e
giorgi tsipuria <giorgi.tsipuria.1@btu.edu.ge>
2024-01-29 20:25:05 +0400

    updated edit route, not css yet

--- index.js
+++ index.js
@@ -8,39 +8,32 @@ app.use(express.static("public"));
 
 const posts = [];
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.render("index.ejs", { posts });
 });
 
-app.post('/add', (req, res) => {
-    const post = req.body.post;
-    posts.push(post);
-    res.redirect('/');
+app.post('/addPost', (req, res) => {
+    const index = req.body.index;
+    const editPost = req.body.editPost;
+
+    if (index !== undefined && index >= 0 && index < posts.length) {
+        posts[index] = editPost;
+    } else {
+        const newPost = req.body.post;
+        posts.push(newPost);
+    }
+
+    res.redirect("/");
 });
 
-app.post("/edit/:ed", (req, res) => {
-    const ed = req.body.ed;
-    const editpost = req.body.editpost;
-
-    if (ed !== undefined && ed >= 0 && ed < posts.length) {
-        posts[ed] = editpost;
+app.post("/delete/:index", (req, res) => {
+    const index = req.params.index;
+    if (index >= 0 && index < posts.length) {
+        posts.splice(index, 1);
     }
     res.redirect("/");
 });
 
-app.post("/delete/:dele", (req, res) => {
-    const dele = req.params.dele;
-    if (dele >= 0 && dele < posts.length) {
-        posts.splice(dele, 1);
-    }
-    res.redirect('/');
-});
-
-
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
-
